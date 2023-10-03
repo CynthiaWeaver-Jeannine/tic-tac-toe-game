39fdf6d7881fa8e4e3c25529c3896501dbf8b88f
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-10-03 18:13:49 -0400

    fix issues with checkForDraw, handleAIMove, handleBoxClick; added getAIMove

--- script.js
+++ script.js
@@ -4,24 +4,38 @@ window.onload = function () {
 	// tic-tac-toe game configuration
 	const humanSymbol = "X";
 	const aiSymbol = "O";
-	// winning combinations
-	const winner = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
 
+	function hasWon(board, player) {
+		const winningCombinations = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		return checkForWinner(board, player);
+		// check if the player has won
+		function checkForWinner(board, player) {
+			return winningCombinations.some(
+				(combination) =>
+					board[combination[0]] === player &&
+					board[combination[1]] === player &&
+					board[combination[2]] === player,
+			);
+		}
+	}
+
+	function isBoardFull(board) {
+		return board.every(cell => cell === "X" || cell === "O");
+	}
 	// initial state
 	let filled = Array(9).fill(false);
 	let boardSymbols = Array(9).fill("");
 	let turn = 1;
 	let gameOver = false;
-
 	// event listeners
 	document.getElementById("gameMode").addEventListener("change", function (e) {
 		const validModes = ["classic", "easy", "random"];
@@ -30,12 +44,10 @@ window.onload = function () {
 			e.target.value = "classic";
 		}
 	});
-
 	document.getElementById("newGameButton").addEventListener("click", newGame);
 	document.getElementById("board").addEventListener("click", function (e) {
 		handleBoxClick(e.target.id);
 	});
-
 	// reset the game
 	function newGame() {
 		for (let i = 1; i <= 9; i++) {
@@ -52,37 +64,43 @@ window.onload = function () {
 		gameOver = false;
 		document.getElementById("result").innerText = "";
 	}
-
+	function checkForDraw() {
+		if (turn >= 9 && !gameOver) {
+			document.getElementById("result").innerText =
+				"It's a Draw! Click NEW GAME!";
+			gameOver = true;
+		}
+	}
 	// handle user's box clicks; manage the game state
 	function handleBoxClick(boxId) {
 		const num = parseInt(boxId.replace("canvas", "")) - 1;
 		const box = document.getElementById(boxId);
 		const context = box.getContext("2d");
 
-		if (!filled[num] && !gameOver) {
-			if (turn % 2 !== 0) {
-				drawSymbolOnBox(context, humanSymbol, box);
-				if (checkForWinner(boardSymbols, humanSymbol)) {
-					document.getElementById(
-						"result",
-					).innerText = `Player ${humanSymbol} won!`;
-					gameOver = true;
-				} else if (turn >= 9) {
-					document.getElementById("result").innerText = "It's a Draw!";
-				} else {
+		if (filled[num] || gameOver) {
+			alert(
+				"This box was already filled or the game is over. Please click on another box or start a new game.",
+			);
+			return;
+		}
+
+		if (turn % 2 !== 0) {
+			// Human's turn
+			drawSymbolOnBox(context, humanSymbol, box);
+
+			if (hasWon(boardSymbols, humanSymbol)) {
+				document.getElementById(
+					"result",
+				).innerText = `Player ${humanSymbol} won!`;
+				gameOver = true;
+			} else {
+				checkForDraw();
+				if (!gameOver) {
 					handleAIMove();
-					if (turn >= 9 && !gameOver) {
-						document.getElementById("result").innerText =
-							"It's a Draw! Click NEW GAME!";
-						gameOver = true;
-					}
 				}
 			}
-		} else {
-			alert("This box was already filled. Please click on another one.");
 		}
 	}
-
 	// draw the symbol on the box and update the game state
 	function drawSymbolOnBox(context, player, box) {
 		if (player === humanSymbol) {
@@ -110,17 +128,7 @@ window.onload = function () {
 		filled[parseInt(box.id.replace("canvas", "")) - 1] = true;
 		turn++;
 	}
-
-	// check if the player has won
-	function checkForWinner(board, player) {
-		return winner.some(
-			(combination) =>
-				board[combination[0]] === player &&
-				board[combination[1]] === player &&
-				board[combination[2]] === player,
-		);
-	}
-
+	
 	// get the empty boxes
 	function getEmptyBoxPositions(currentBoardState) {
 		const emptyPositions = [];
@@ -135,28 +143,25 @@ window.onload = function () {
 		return emptyPositions;
 	}
 
-	/* 
-		Minimax algorithm with alpha-beta pruning
-		https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
-	*/
+	//Minimax algorithm with alpha-beta pruning	
 	function evaluateBestMove(
 		currentBoardState,
 		player,
 		alpha = -Infinity,
 		beta = Infinity,
 	) {
-		const emptyPositions = getEmptyBoxPositions(boardSymbols);
-		if (emptyPositions.length === 0) {
-			console.log("No more moves left!");
-			return;
-		}
-		if (checkForWinner(currentBoardState, humanSymbol)) {
+		const emptyPositions = getEmptyBoxPositions(currentBoardState);
+
+		if (hasWon(currentBoardState, humanSymbol)) {
 			return { score: -10 };
-		} else if (checkForWinner(currentBoardState, aiSymbol)) {
+		}
+		if (hasWon(currentBoardState, aiSymbol)) {
 			return { score: 10 };
-		} else if (emptyPositions.length === 0) {
+		}
+		if (emptyPositions.length === 0) {
 			return { score: 0 };
 		}
+
 		if (player === aiSymbol) {
 			let bestScore = -Infinity;
 			let bestMove;
@@ -181,7 +186,7 @@ window.onload = function () {
 					break;
 				}
 			}
-			return bestMove;
+			return bestMove || { score: -Infinity };
 		} else {
 			let bestScore = Infinity;
 			let bestMove;
@@ -206,52 +211,42 @@ window.onload = function () {
 					break;
 				}
 			}
-			return bestMove;
+			return bestMove || { score: Infinity };
 		}
 	}
 
-	// handle the AI's move; check for winner or draw
 	function handleAIMove() {
-		let nextId;
-		const gameMode = document.getElementById("gameMode").value;
+		const boxId = getAIMove();
+		const box = document.getElementById(boxId);
+		const context = box.getContext("2d");
 
-		switch (gameMode) {
+		if (gameOver) {
+			alert("The game is over. Click NEW GAME to play again!");
+			return;
+		}
+
+		drawSymbolOnBox(context, aiSymbol, box);
+
+		if (hasWon(boardSymbols, aiSymbol)) {
+			document.getElementById("result").innerText = "Max Won! Click NEW GAME!";
+			gameOver = true;
+		} else {
+			checkForDraw();
+		}
+	}
+
+	function getAIMove() {
+		switch (document.getElementById("gameMode").value) {
 			case "classic":
-				const nextMove = evaluateBestMove(boardSymbols, aiSymbol);
-				nextId = "canvas" + (nextMove.id + 1);
-				break;
+				return "canvas" + (evaluateBestMove(boardSymbols, aiSymbol).id + 1);
 			case "easy":
-				nextId = getEasyMove();
-				break;
+				return getEasyMove();
 			case "random":
-				nextId = getRandomMove();
-				break;
+				return getRandomMove();
 			default:
 				console.error("Unknown game mode:", gameMode);
 				alert("Invalid game mode selected. Please choose a valid game mode.");
 				return;
-		}
-
-		const box = document.getElementById(nextId);
-		const context = box.getContext("2d");
-
-		if (!gameOver) {
-			if (!gameOver) {
-				if (turn % 2 !== 0) {
-					drawSymbolOnBox(context, humanSymbol, box);
-					drawSymbolOnBox(context, aiSymbol, box);
-					if (checkForWinner(boardSymbols, aiSymbol)) {
-						document.getElementById("result").innerText =
-							"Max Won! Click NEW GAME!";
-						gameOver = true;
-					} else if (turn >= 9) {
-						document.getElementById("result").innerText =
-							"It's a Draw! Click NEW GAME!";
-					}
-				} else {
-					alert("The game is over. Click NEW GAME to play!");
-				}
-			}
 		}
 	}
 
@@ -266,7 +261,6 @@ window.onload = function () {
 				boardSymbols[i] = "";
 			}
 		}
-
 		// If no winning move, choose a random empty box
 		return getRandomMove();
 	}

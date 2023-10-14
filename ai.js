c192e201e699b4dd8eb5271d761c75a232478991
CYNTHIA J WEAVER <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-10-14 09:57:33 -0400

    3x3 and 4x4 (#2)
    * updated game-checking logic for to implement 4x4 game or larger
    
    * initialize filled and boardSymbols arrays based on board size
    
    * added logic for the 4x4 board
    
    * added styling and structure for switching between boards
    
    * fix issues with too many magic numbers; add styling and logic for multiple boards

--- ai.js
+++ ai.js
@@ -1,55 +1,138 @@
 /** @format */
 const humanSymbol = "X";
 const aiSymbol = "O";
+const HUMAN_WIN_SCORE = -10;
+const AI_WIN_SCORE = 10;
+const DRAW_SCORE = 0;
+export let boardConfig = {
+	size: 4
+}
+
+function generateWinningCombinations() {
+	let combinations = [];
+	// rows
+	for (let i = 0; i < boardConfig.size; i++) {
+		combinations.push(
+			[...Array(boardConfig.size).keys()].map((j) => i * boardConfig.size + j),
+		);
+	}
+	// columns
+	for (let j = 0; j < boardConfig.size; j++) {
+		combinations.push(
+			[...Array(boardConfig.size).keys()].map((i) => i * boardConfig.size + j),
+		);
+	}
+	//diagonals
+	combinations.push(
+		[...Array(boardConfig.size).keys()].map((i) => i * boardConfig.size + i),
+	);
+	combinations.push(
+		[...Array(boardConfig.size).keys()].map(
+			(i) => i * boardConfig.size + (boardConfig.size - 1 - i),
+		),
+	);
+
+	return combinations;
+}
 
 function hasWon(board, player) {
-	const winningCombinations = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
+	const winningCombinations = generateWinningCombinations();
 
 	return checkForWinner(board, player);
 
 	function checkForWinner(board, player) {
-		return winningCombinations.some(
-			(combination) =>
-				board[combination[0]] === player &&
-				board[combination[1]] === player &&
-				board[combination[2]] === player,
+		return winningCombinations.some((combination) =>
+			combination.every((index) => board[index] === player),
 		);
 	}
 }
-//Minimax algorithm with alpha-beta pruning
+
+function heuristicScore(board) {
+	let score = 0;
+	const winningCombinations = generateWinningCombinations();
+	for (let combination of winningCombinations) {
+		const symbols = combination.map((index) => board[index]);
+		// Calculate AI's potential wins
+		if (
+			symbols.filter((symbol) => symbol === aiSymbol).length ===
+				boardConfig.size - 1 &&
+			symbols.filter((symbol) => symbol === "").length === 1
+		) {
+			score += 3;
+		}
+
+		// Calculate Human's potential wins
+		if (
+			symbols.filter((symbol) => symbol === humanSymbol).length ===
+				boardConfig.size - 1 &&
+			symbols.filter((symbol) => symbol === "").length === 1
+		) {
+			score -= 4; // Note: Higher penalty for human potential wins to prioritize blocking
+		}
+
+		// Prioritize center for larger boards
+		if (boardConfig.size > 3) {
+			const centerIndexes = [
+				Math.floor(boardConfig.size / 2) * boardConfig.size +
+					Math.floor(boardConfig.size / 2),
+				Math.floor(boardConfig.size / 2) * boardConfig.size +
+					Math.floor(boardConfig.size / 2) -
+					1,
+				(Math.floor(boardConfig.size / 2) - 1) * boardConfig.size +
+					Math.floor(boardConfig.size / 2),
+				(Math.floor(boardConfig.size / 2) - 1) * boardConfig.size +
+					Math.floor(boardConfig.size / 2) -
+					1,
+			];
+			for (const centerIndex of centerIndexes) {
+				if (board[centerIndex] === aiSymbol) {
+					score += 1;
+				} else if (board[centerIndex] === humanSymbol) {
+					score -= 1;
+				}
+			}
+		}
+	}
+	return score;}
+
+// Minimax algorithm with alpha-beta pruning;
+// includes depth-limiting logic
 function evaluateBestMove(
 	currentBoardState,
 	player,
+	depth = 0,
 	alpha = -Infinity,
 	beta = Infinity,
+	maxDepth = 6,
 ) {
 	const emptyPositions = getEmptyBoxPositions(currentBoardState);
 	if (hasWon(currentBoardState, humanSymbol)) {
-		return { score: -10 };
+		return { score: HUMAN_WIN_SCORE };
 	}
 	if (hasWon(currentBoardState, aiSymbol)) {
-		return { score: 10 };
+		return { score: AI_WIN_SCORE };
 	}
 	if (emptyPositions.length === 0) {
-		return { score: 0 };
+		return { score: DRAW_SCORE };
+	}
+	if (depth >= maxDepth) {
+		return { score: heuristicScore(currentBoardState) };
 	}
 	if (player === aiSymbol) {
 		let bestScore = -Infinity;
 		let bestMove;
 		for (let emptyIndex of emptyPositions) {
 			currentBoardState[emptyIndex] = player;
+			const expectedLength = boardConfig.size * boardConfig.size;
+			if (currentBoardState.length !== expectedLength) {
+				console.error(
+					"Inconsistency detected in currentBoardState length during evaluation!",
+				);
+			}
 			let currentScore = evaluateBestMove(
 				currentBoardState,
 				humanSymbol,
+				depth+1,
 				alpha,
 				beta,
 			).score;
@@ -72,6 +155,7 @@ function evaluateBestMove(
 			let currentScore = evaluateBestMove(
 				currentBoardState,
 				aiSymbol,
+				depth+1,
 				alpha,
 				beta,
 			).score;
@@ -138,6 +222,8 @@ function getRandomMove(boardSymbols) {
 }
 
 export {
+	humanSymbol,
+	aiSymbol,
 	hasWon,
 	evaluateBestMove,
 	getEmptyBoxPositions,
--- main.js
+++ main.js
@@ -1,51 +1,138 @@
+/** @format */
 
-import { hasWon, evaluateBestMove, getEmptyBoxPositions, getAIMove, getEasyMove, getRandomMove} from "./ai.js"
+import {
+	humanSymbol,
+	aiSymbol,
+	boardConfig,
+	hasWon,
+	getAIMove
+} from "./ai.js";
 
-
-window.onload = function () {
-	let humanSymbol = "X";
-	let aiSymbol = "O";
+window.addEventListener("DOMContentLoaded", function () {
 	let humanWinCount = 0;
 	let aiWinCount = 0;
+	const FIRST_PLAYER_CHANCE = 0.5;
 
 	// initial state
-	let filled = Array(9).fill(false);
-	let boardSymbols = Array(9).fill("");
+	let filled = Array(boardConfig.size * boardConfig.size).fill(false);
+	let boardSymbols = Array(boardConfig.size * boardConfig.size).fill("");
 	let turn = 1;
 	let gameOver = false;
 
-	function drawSymbolOnBox(context, player, box) {
-		if (player === humanSymbol) {
-			box.style.backgroundColor = "#fb5181";
-			context.beginPath();
-			context.moveTo(15, 15);
-			context.lineTo(85, 85);
-			context.moveTo(85, 15);
-			context.lineTo(15, 85);
-			context.lineWidth = 21;
-			context.lineCap = "round";
-			context.strokeStyle = "white";
-			context.stroke();
-			context.closePath();
-		} else {
-			box.style.backgroundColor = "#71e29c";
-			context.beginPath();
-			context.arc(50, 50, 35, 0, 2 * Math.PI);
-			context.lineWidth = 20;
-			context.strokeStyle = "white";
-			context.stroke();
-			context.closePath();
+	function consistencyCheck() {
+		if (filled.length !== boardConfig.size * boardConfig.size) {
+			console.error('Inconsistency detected in "filled" array size!');
 		}
-		boardSymbols[parseInt(box.id.replace("canvas", "")) - 1] = player;
-		filled[parseInt(box.id.replace("canvas", "")) - 1] = true;
-		turn++;
+		if (boardSymbols.length !== boardConfig.size * boardConfig.size) {
+			console.error('Inconsistency detected in "boardSymbols" array size!');
+		}
 	}
 
+	function clearOldBoard() {
+		for (let i = 1; i <= boardConfig.size * boardConfig.size; i++) {
+			const canvas = document.getElementById("canvas" + i);
+			if (canvas) {
+				// Check if the canvas element exists
+				const context = canvas.getContext("2d");
+				context.clearRect(0, 0, canvas.width, canvas.height);
+				canvas.style.backgroundColor = "";
+			}
+		}
+	}
+
+	function generateBoard(size) {
+		const boardDiv = document.getElementById("board");
+		boardDiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+		boardDiv.innerHTML = "";
+		for (let i = 1; i <= size * size; i++) {
+			const canvas = document.createElement("canvas");
+			canvas.id = "canvas" + i;
+			boardDiv.appendChild(canvas);
+		}
+		console.log("Current board size:", boardConfig.size);
+	}
+
+	function resetBoardAndGameState() {
+		clearOldBoard();
+
+		filled = Array(boardConfig.size * boardConfig.size).fill(false);
+		boardSymbols = Array(boardConfig.size * boardConfig.size).fill("");
+		gameOver = false;
+		document.getElementById("result").innerText = "";
+
+		generateBoard(boardConfig.size);
+		consistencyCheck();
+	}
+
+function drawSymbolOnBox(context, player, box) {
+	const canvasWidth = context.canvas.width;
+	const canvasHeight = context.canvas.height;
+
+	const lineWidth = canvasWidth * 0.1;
+	const margin = canvasWidth * 0.15;
+	const endPositionWidth = canvasWidth - margin;
+	const endPositionHeight = canvasHeight - margin;
+
+	if (player === humanSymbol) {
+		box.style.backgroundColor = "#fb5181";
+		context.beginPath();
+		context.moveTo(margin, margin);
+		context.lineTo(endPositionWidth, endPositionHeight);
+		context.moveTo(endPositionWidth, margin);
+		context.lineTo(margin, endPositionHeight);
+		context.lineWidth = lineWidth;
+		context.lineCap = "round";
+		context.strokeStyle = "white";
+		context.stroke();
+	} else {
+		box.style.backgroundColor = "#71e29c";
+		const circleCenterWidth = canvasWidth / 2;
+		const circleCenterHeight = canvasHeight / 2;
+		const circleRadius = Math.min(canvasWidth, canvasHeight) / 2 - lineWidth;
+
+		context.beginPath();
+		context.arc(
+			circleCenterWidth,
+			circleCenterHeight,
+			circleRadius,
+			0,
+			2 * Math.PI,
+		);
+		context.lineWidth = lineWidth;
+		context.lineCap = "round";
+		context.strokeStyle = "white";
+		context.stroke();
+	}
+
+	boardSymbols[parseInt(box.id.replace("canvas", "")) - 1] = player;
+	filled[parseInt(box.id.replace("canvas", "")) - 1] = true;
+	turn++;
+}
+
+
+
+		
 	function isBoardFull(board) {
 		return board.every((cell) => cell === "X" || cell === "O");
 	}
 
 	// event listeners
+	document.addEventListener("DOMContentLoaded", function () {
+	
+	});
+
+	const radioButtons = document.querySelectorAll('input[name="boardSize"]');
+	radioButtons.forEach((button) => {
+		button.addEventListener("change", function (event) {
+			const selectedSize = parseInt(event.target.value);
+			boardConfig.size = selectedSize;
+			console.log("Current board sizeline72:", boardConfig.size);
+			resetBoardAndGameState();
+			consistencyCheck();
+			generateBoard(selectedSize);
+			newGame();
+		});
+	});
 	document.getElementById("gameMode").addEventListener("change", function (e) {
 		const validModes = ["classic", "easy", "random"];
 		if (!validModes.includes(e.target.value)) {
@@ -66,20 +153,51 @@ window.onload = function () {
 		document.getElementById("aiWins").innerText = aiWinCount;
 		document.getElementById("draws").innerText = "0";
 	});
+	function handleBoardSizeChange() {
+		// Clear the board
+		clearBoard();
+
+		// Reset the game state
+		resetGameState();
+
+		// Depending on the selected size, re-render the board
+		let boardSize = 3; // default to 3x3
+		if (document.getElementById("size4").checked) {
+			boardSize = 4;
+		}
+		renderBoard(boardSize);
+	}
+
+	function clearBoard() {
+		const boardElement = document.getElementById("board");
+		while (boardElement.firstChild) {
+			boardElement.removeChild(boardElement.lastChild);
+		}
+	}
+
+	function resetGameState() {
+		// Implement resetting your game's state here, like resetting turns, scores, etc.
+		// Depending on how you've implemented the game logic, this function will vary.
+	}
+
+	function renderBoard(size) {
+		const boardElement = document.getElementById("board");
+		for (let i = 0; i < size * size; i++) {
+			const canvas = document.createElement("canvas");
+			canvas.id = "canvas" + (i + 1);
+			boardElement.appendChild(canvas);
+		}
+		// Note: You might also want to attach event listeners to these new canvas elements or set up their initial styles.
+	}
 
 	// reset the game
 	function newGame() {
-		for (let i = 1; i <= 9; i++) {
-			const canvas = document.getElementById("canvas" + i);
-			const context = canvas.getContext("2d");
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			canvas.style.backgroundColor = "";
-			gameOver = false;
-		}
-		filled.fill(false);
-		boardSymbols.fill("");
-		document.getElementById("result").innerText = "";
-		const randomPlayer = Math.random() < 0.5 ? "human" : "ai";
+		resetBoardAndGameState();
+		consistencyCheck();
+
+		console.log("Current board sizeline140:", boardConfig.size);
+
+		const randomPlayer = Math.random() < FIRST_PLAYER_CHANCE ? "human" : "ai";
 		if (randomPlayer === "human") {
 			turn = 1;
 		} else {
@@ -100,13 +218,16 @@ window.onload = function () {
 
 	// handle user's box clicks; manage the game state
 	function handleBoxClick(boxId) {
+		console.log("Human clicked on:", boxId);
 		const num = parseInt(boxId.replace("canvas", "")) - 1;
 		const box = document.getElementById(boxId);
 		const context = box.getContext("2d");
-		if (filled[num] || gameOver) {
-			alert(
-				"If the box is filled, choose a differnt one. \nIf the game is over, click NEW GAME!",
-			);
+		if (filled[num]) {
+			alert("This box is filled, please choose a differnt one.");
+			return;
+		}
+		if (gameOver) {
+			alert("The game is over. Click NEW GAME to play again!");
 			return;
 		}
 		if (turn % 2 !== 0) {
@@ -128,7 +249,9 @@ window.onload = function () {
 	}
 	// handle AI's move
 	function handleAIMove() {
-		const boxId = getAIMove(boardSymbols);			
+		const boxId = getAIMove(boardSymbols);
+		console.log("Returned boxId from getAIMove:", boxId);
+		console.log(boxId);
 		const box = document.getElementById(boxId);
 		const context = box.getContext("2d");
 		if (gameOver) {
@@ -145,10 +268,12 @@ window.onload = function () {
 			checkForDraw();
 		}
 	}
-}
 
-        
+	generateBoard(boardConfig.size);
+	console.log("Current board sizeline221:", boardConfig.size);
+
+	newGame();
+	document.getElementById("size" + boardConfig.size).checked = true;
 
 
-
-	
+});
